f1aa5f10413314a790aa16ea6ad5ae4ae22773e8
NurKeinNeid <mralexman3000@gmail.com>
2022-01-09 21:45:03 +0100

    miatoll: Remove some packages
    Change-Id: I1f318ae8f0263177040e10baa25952c1accdea32
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- device.mk
+++ device.mk
@@ -340,6 +340,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
+# Remove packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
