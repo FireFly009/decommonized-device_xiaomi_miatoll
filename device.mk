a3dcfa729d69fed21dbd8c2a3b0cc814234e6e1e
zlewchan <x@zlew.tf>
2022-09-11 15:14:05 +0200

    miatoll: Bring back in-tree fingerprint HAL
    This reverts commit 165b7d83459a8468da1d5ef704a4668592f569e1.

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_atoll
 
 # FM
 PRODUCT_PACKAGES += \
