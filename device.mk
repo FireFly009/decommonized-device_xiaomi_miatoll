9d6d057fac52cae0b8efeb78ec1beb4e4e66ff72
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    miatoll: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -148,7 +148,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
