17f8d8dfd4912ee11796ea25fdce88d2ff952c98
Mathieu Chartier <mathieuc@google.com>
2023-01-26 17:43:42 +0800

    miatoll: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -395,6 +395,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
