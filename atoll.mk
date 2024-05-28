2a67ec46ab2389be9f33a1f38ba498b8a1f5f9b3
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    sm6250-common: Allow to access bootable/deprecated-ota targets
    miatoll is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to miatoll namespace list.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- atoll.mk
+++ atoll.mk
@@ -289,6 +289,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TimeKeep
 
+# Update
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
