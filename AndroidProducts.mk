0c4c04d970d3d516c3a9a1b0ecf295507539f163
FireFly009 <lucifer2163@protonmail.ch>
2024-06-02 17:17:57 +0000

    miatoll: Awakend

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_miatoll.mk
+    $(LOCAL_DIR)/awaken_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_miatoll-user \
-    blaze_miatoll-userdebug \
-    blaze_miatoll-eng
+    awaken_miatoll-user \
+    awaken_miatoll-userdebug \
+    awaken_miatoll-eng
