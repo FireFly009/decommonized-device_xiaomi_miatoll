0eb2c4629f4b7ebd43aa4f4af913f0fe877b709f
clarencelol <clarencekuiek@proton.me>
2024-06-17 21:59:49 +0800

    miatoll: Include common xiaomi FCM
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,7 @@ ART_BUILD_HOST_DEBUG := false
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/awaken/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
